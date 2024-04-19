659a15aeb6ea1ef397306bfaed3d68c865424fc3
kame-sama <ad.bodeev@gmail.com>
2024-04-19 11:53:04 +0500

    add example data

---
+++ src/data/example-data.ts
@@ -0,0 +1,60 @@
+const general = ['John', 'Doe', 'Senior product Designer', '/example.png'];
+
+const contacts = [
+  '+0 000 000 0000',
+  'johndoe@gmail.com',
+  'john-doe',
+  'john-doe',
+  'username.com',
+];
+
+const experience = [
+  [
+    'Job name',
+    '@Company name',
+    '2024-01',
+    '',
+    'Startup SaaS, Business vision, UX methodology, Design system, UI Kit, User testing, Performance',
+  ],
+  [
+    'Job name',
+    '@Company name',
+    '2024-01',
+    '',
+    'Startup SaaS, Business vision, UX methodology, Design system, UI Kit, User testing, Performance',
+  ],
+  [
+    'Job name',
+    '@Company name',
+    '2024-01',
+    '',
+    'Startup SaaS, Business vision, UX methodology, Design system, UI Kit, User testing, Performance',
+  ],
+  [
+    'Job name',
+    '@Company name',
+    '2024-01',
+    '',
+    'Startup SaaS, Business vision, UX methodology, Design system, UI Kit, User testing, Performance',
+  ],
+];
+
+const education = [
+  ['Course name', '@School name', '2023-01', '2024-01'],
+  ['Course name', '@School name', '2023-01', '2024-01'],
+  ['Course name', '@School name', '2023-01', '2024-01'],
+];
+
+const skills = [
+  [
+    'Category',
+    'User research, Quali. & quanti. survey, Solution design, Wireframe & Prototyping, User testing & analysis',
+  ],
+  [
+    'Category',
+    'Interface design, CMS, 3D Modeling, Texture and animation, AR, Integration',
+  ],
+  ['Languages', 'English C2, French C2, Spanish B2'],
+];
+
+export { general, contacts, experience, education, skills };
